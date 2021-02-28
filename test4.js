621ac3b21d63aa9ea48a7bac87c67f7245f3f72f
fujikijun <79777115+fujikijun@users.noreply.github.com>
2021-02-28 17:24:53 +0900

    s

--- test4.js
+++ test4.js
@@ -6,46 +6,17 @@
 
 // for red, green, and blue color values
 let r, g, b;
-var isTouch = false;
-var motionData = [];
-var orientationData = [];
-
-//. iOS 13 対応 : https://bagelee.com/webar-vr/ios13-webar-webvr/
-//.             : https://qiita.com/nakakaz11/items/a9be602874bd54819a18
-function ClickRequestDeviceSensor(){
-  //. ユーザーに「許可」を明示させる必要がある
-  DeviceOrientationEvent.requestPermission().then( function( response ){
-    if( response === 'granted' ){
-      window.addEventListener( "deviceorientation", deviceOrientation );
-      $('#sensorrequest').css( 'display', 'none' );
-      $('#cdiv').css( 'display', 'block' );
-    }
-  }).catch( function( e ){
-    console.log( e );
-  });
-
-  DeviceMotionEvent.requestPermission().then( function( response ){
-    if( response === 'granted' ){
-      window.addEventListener( "devicemotion", deviceMotion );
-      $('#sensorrequest').css( 'display', 'none' );
-      $('#cdiv').css( 'display', 'block' );
-    }
-  }).catch( function( e ){
-    console.log( e );
-  });
-}
 
 function setup() {
   createCanvas(720, 400);
   // Pick colors randomly
   r = random(255);
   g = random(255);
-  b = random(255);
-  
+  b = random(255);  
 }
 
 function draw() {
-  background(127,0,0);
+  background(127,127,0);
   // Draw a circle
   strokeWeight(2);
   stroke(r, g, b);
