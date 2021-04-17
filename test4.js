fb047fe7ac18fd33af266ee042dde887f1aa4cc4
fujikijun <79777115+fujikijun@users.noreply.github.com>
2021-04-17 11:21:46 +0900

    Update test4.js

--- test4.js
+++ test4.js
@@ -112,7 +112,7 @@ function deviceMotion( e )
     g = e.acceleration.y*60+127;
     //b = e.accelerationIncludingGravity.z*60+127;
     b = 0;
-    text( g, 20, 20 );
+    text( g, 20, 200 );
     
     ball.xSpeed += e.accelerationIncludingGravity.x*0.1;
     ball.ySpeed -= e.accelerationIncludingGravity.y*0.1;
